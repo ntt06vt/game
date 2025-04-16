4c2bce42b63773c18cbcbf6c7e723ee1cabca1f8
ntt06vt <tung03022006@gmail.com>
2025-04-16 15:05:47 +0700

    Update defs.h

--- defs.h
+++ defs.h
@@ -3,8 +3,17 @@
 
 const int SCREEN_WIDTH = 1500;
 const int SCREEN_HEIGHT = 1000;
-const char* WINDOW_TITLE = "Hello World!";
-const char* a[] = {
+const char* WINDOW_TITLE = "Game";
+const char* background="nen.jpg";
+const char* title="timhinhgiongnhau.png";
+const char* playbutton="choi.png";
+const char* unknowimage="khung.png";
+const char* win="win.png";
+const char* music1="satisfying-lofi-for-focus-study-amp-working-242103.mp3";
+const char* music2="jump.wav";
+const char* music3="winning-218995.mp3";
+int board[4][4]={{0,0,1,1},{2,2,3,3},{4,4,5,5},{6,6,7,7}};
+const char* image[] = {
     "a0.png",
     "a1.png",
     "a2.png",
@@ -14,5 +23,6 @@ const char* a[] = {
     "a6.png",
     "a7.png"
 };
+
 
 #endif
